bdffea34f0905c5df745f3d175de057451d56d7b
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-20 15:18:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,6 @@
 //define functions here
 
+<<<<<<< HEAD
 //bind click event to the p tag, alert "Hey!" when Paragraph clicked.
 function getIt() {
 $('p').on('click', function () {   
@@ -14,6 +15,38 @@ $('img').on('load',function() {
   $('img').addClass("tasty");
 })
 }
+=======
+
+function getIt() {
+    alert("Hey!")     //bind click event to the p tag, alert "Hey!" when Paragraph clicked.
+}
+
+$('p').click(getIt)
+
+// binds load event that adds the class "tasty" to the image to add a red frame around it.
+function frameIt() {
+  $('img').addClass("tasty");
+}
+
+$('img').on('load',getframeIt);
+
+
+function pressIt(e) {
+  //binds an event that alerts the user when the G key was pressed down
+  if (e.which == 71) {
+  alert("G key pressed");
+  }
+}
+
+$('#typing').on('keydown', pressIt);
+
+function submitIt() {
+  //binds an event that alerts "Your form is going to be submitted now." when the form is submitted
+}
+
+$(document).ready(function(){
+  
+>>>>>>> 429d7818e5504235064a040ed00cc12b40cfbf4e
 
 function pressIt(key) {
   //binds an event that alerts the user when the G key was pressed down
@@ -24,6 +57,7 @@ function pressIt(key) {
   })
 }
 
+<<<<<<< HEAD
 function submitIt() {
   //binds an event that alerts "Your form is going to be submitted now." when the form is submitted
  $('form').on('submit', function() {
@@ -34,3 +68,7 @@ function submitIt() {
 $(document).ready(function(){
   //call functions here
 })
+=======
+
+});
+>>>>>>> 429d7818e5504235064a040ed00cc12b40cfbf4e
